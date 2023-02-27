af03cd5f9561c3cce2bd9385e24bf5ab44cbbb60
MaxWaldorf <tigrou40@hotmail.com>
2023-02-27 19:47:45 +0100

    revamp again

--- library.js
+++ library.js
@@ -1,145 +1,256 @@
 'use strict';
 
-const passport = module.parent.require('passport');
-const nconf = module.parent.require('nconf');
-const winston = module.parent.require('winston');
+(function (module) {
+	/*
+		Welcome to the SSO OAuth plugin! If you're inspecting this code, you're probably looking to
+		hook up NodeBB with your existing OAuth endpoint.
 
-const { Issuer, Strategy, custom } = require('openid-client');
+		Step 1: Fill in the "constants" section below with the requisite informaton. Either the "oauth"
+				or "oauth2" section needs to be filled, depending on what you set "type" to.
 
-const AUTH_OIDC_BASE_PATH = '/auth/oidc';
-const AUTH_OIDC_LOGIN_PATH = `${AUTH_OIDC_BASE_PATH}/login`;
-const AUTH_OIDC_CALLBACK_PATH = `${AUTH_OIDC_BASE_PATH}/callback`;
-const CLOCK_TOLERANCE = 10;
+		Step 2: Give it a whirl. If you see the congrats message, you're doing well so far!
 
-const controllers = require('./lib/controllers');
+		Step 3: Customise the `parseUserReturn` method to normalise your user route's data return into
+				a format accepted by NodeBB. Instructions are provided there. (Line 146)
 
-const { UserHelper, SettingsHelper } = require('./lib/helpers');
+		Step 4: If all goes well, you'll be able to login/register via your OAuth endpoint credentials.
+	*/
 
-/**
-		* Initializes the plugin
-		* @param {Object} data
-		* @param {Function} callback
-		*/
-async function init({ router, middleware }, callback) {
-	try {
-		winston.info('Setting up OpenID Connect UI routes...');
+	const User = require.main.require('./src/user');
+	const Groups = require.main.require('./src/groups');
+	const db = require.main.require('./src/database');
+	const authenticationController = require.main.require('./src/controllers/authentication');
 
-		router.get('/admin/plugins/oidc', middleware.admin.buildHeader, controllers.renderAdminPage);
-		router.get('/api/admin/plugins/oidc', controllers.renderAdminPage);
+	const async = require('async');
 
-		callback();
-	} catch (err) {
-		callback(err);
-	}
-}
+	const passport = module.parent.require('passport');
+	const nconf = module.parent.require('nconf');
+	const winston = module.parent.require('winston');
 
-/**
-		* Adds menu item in admin.
-		* @param {Object} header
-		* @param {Function} callback
-		*/
-async function addMenuItem(header, callback) {
-	header.authentication.push({
-		route: '/plugins/oidc',
-		icon: 'fa-key',
-		name: 'OpenID Connect',
+	/**
+	 * REMEMBER
+	 *   Never save your OAuth Key/Secret or OAuth2 ID/Secret pair in code! It could be published and leaked accidentally.
+	 *   Save it into your config.json file instead:
+	 *
+	 *   {
+	 *     ...
+	 *     "oauth": {
+	 *       "id": "someoauthid",
+	 *       "secret": "youroauthsecret"
+	 *     }
+	 *     ...
+	 *   }
+	 *
+	 *   ... or use environment variables instead:
+	 *
+	 *   `OAUTH__ID=someoauthid OAUTH__SECRET=youroauthsecret node app.js`
+	 */
+
+	const constants = Object.freeze({
+		type: 'oauth2',	// Either 'oauth' or 'oauth2'
+		name: 'keycloak',	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
+		oauth: {
+			requestTokenURL: '',
+			accessTokenURL: '',
+			userAuthorizationURL: '',
+			consumerKey: nconf.get('oauth:key'),	// don't change this line
+			consumerSecret: nconf.get('oauth:secret'),	// don't change this line
+		},
+		oauth2: {
+			authorizationURL: nconf.get('oauth:auth'),
+			tokenURL: nconf.get('oauth:token'),
+			clientID: nconf.get('oauth:id'),	// don't change this line
+			clientSecret: nconf.get('oauth:secret'),	// don't change this line
+		},
+		userRoute: nconf.get('oauth:userinfo'),	// This is the address to your app's "user profile" API endpoint (expects JSON)
 	});
-	callback(null, header);
-}
 
-/**
-		* Finds or Creates the logged user as needed.
-		* @param {TokenSet} tokenSet
-		* @param {Object} profile
-		* @param {Function} callback
-		*/
-async function verify(tokenSet, profile, callback) {
-	try {
-		const claims = tokenSet.claims();
-		let uid = await UserHelper.getUidByIssuerAndSubject(claims.iss, claims.sub);
+	const OAuth = {};
+	let configOk = false;
+	let passportOAuth;
+	let opts;
 
-		if (uid) {
-			callback(null, { uid });
-			return;
-		}
-
-		uid = await UserHelper.create({
-			username: claims.preferred_username || claims.email,
-			email: String(claims.email).toLowerCase(),
-			oidcIssuer: claims.iss,
-			oidcSubject: claims.sub,
-		});
-
-		callback(null, { uid });
-	} catch (err) {
-		callback(err);
+	if (!constants.name) {
+		winston.error('[sso-oauth] Please specify a name for your OAuth provider (library.js:32)');
+	} else if (!constants.type || (constants.type !== 'oauth' && constants.type !== 'oauth2')) {
+		winston.error('[sso-oauth] Please specify an OAuth strategy to utilise (library.js:31)');
+	} else if (!constants.userRoute) {
+		winston.error('[sso-oauth] User Route required (library.js:31)');
+	} else {
+		configOk = true;
 	}
-}
 
-/**
-		* Creates the Passport Strategy to login with OpenID Connect.
-		*
-		* @param {Array} strategies
-		* @param {Function} callback
-		*/
-async function getStrategy(strategies, callback) {
-	try {
-		const config = await SettingsHelper.get('oidc');
+	OAuth.getStrategy = function (strategies, callback) {
+		if (configOk) {
+			passportOAuth = require('passport-oauth')[constants.type === 'oauth' ? 'OAuthStrategy' : 'OAuth2Strategy'];
 
-		if (!config.discoverUrl || !config.clientId || !config.clientSecret) {
-			winston.warn('[oidc] OpenID Connect configuration missing, disabling.');
+			if (constants.type === 'oauth') {
+				// OAuth options
+				opts = constants.oauth;
+				opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
+
+				passportOAuth.Strategy.prototype.userProfile = function (token, secret, params, done) {
+
+					// If your OAuth provider requires the access token to be sent in the query  parameters
+					// instead of the request headers, comment out the next line:
+					this._oauth._useAuthorizationHeaderForGET = true;
+
+					this._oauth.get(constants.userRoute, token, secret, function (err, body/* , res */) {
+						if (err) {
+							return done(err);
+						}
+
+						try {
+							var json = JSON.parse(body);
+							OAuth.parseUserReturn(json, function (err, profile) {
+								if (err) return done(err);
+								profile.provider = constants.name;
+
+								done(null, profile);
+							});
+						} catch (e) {
+							done(e);
+						}
+					});
+				};
+			} else if (constants.type === 'oauth2') {
+				// OAuth 2 options
+				opts = constants.oauth2;
+				opts.callbackURL = nconf.get('url') + '/auth/' + constants.name + '/callback';
+
+				passportOAuth.Strategy.prototype.userProfile = function (accessToken, done) {
+
+					// If your OAuth provider requires the access token to be sent in the query  parameters
+					// instead of the request headers, comment out the next line:
+					this._oauth2._useAuthorizationHeaderForGET = true;
+
+					this._oauth2.get(constants.userRoute, accessToken, function (err, body/* , res */) {
+						if (err) {
+							return done(err);
+						}
+
+						try {
+							var json = JSON.parse(body);
+							OAuth.parseUserReturn(json, function (err, profile) {
+								if (err) return done(err);
+								profile.provider = constants.name;
+
+								done(null, profile);
+							});
+						} catch (e) {
+							done(e);
+						}
+					});
+				};
+			}
+
+			opts.passReqToCallback = true;
+
+			passport.use(constants.name, new passportOAuth(opts, async (req, token, secret, profile, done) => {
+				const user = await OAuth.login({
+					oAuthid: profile.id,
+					handle: profile.displayName,
+					email: profile.emails[0].value,
+					isAdmin: profile.isAdmin,
+				});
+
+				authenticationController.onSuccessfulLogin(req, user.uid);
+				done(null, user);
+			}));
+
+			strategies.push({
+				name: constants.name,
+				url: '/auth/' + constants.name,
+				callbackURL: '/auth/' + constants.name + '/callback',
+				icon: 'fa-check-square',
+				scope: (constants.scope || '').split(','),
+			});
+
 			callback(null, strategies);
-			return;
+		} else {
+			callback(new Error('OAuth Configuration is invalid'));
 		}
-		winston.verbose('[oidc] Fetching OpenID Connect Issuer informations ...');
+	};
 
-		const issuer = await Issuer.discover(config.discoverUrl);
+	OAuth.parseUserReturn = function (data, callback) {
+		// Alter this section to include whatever data is necessary
+		// NodeBB *requires* the following: id, displayName, emails.
+		// Everything else is optional.
 
-		winston.verbose('[oidc] Creating OpenID Connect Passport Strategy ...');
+		// Find out what is available by uncommenting this line:
+		// console.log(data);
 
-		const client = new issuer.Client({
-			client_id: config.clientId,
-			client_secret: config.clientSecret,
-			redirect_uris: [nconf.get('url') + AUTH_OIDC_CALLBACK_PATH],
-		});
+		var profile = {};
+		profile.id = data.id;
+		profile.displayName = data.name;
+		profile.emails = [{ value: data.email }];
 
-		// Adding some timestamp tolerance as they may be a little different
-		// if nodebb and the OpenID Connect server are on two separate
-		// hosts.
-		client[custom.clock_tolerance] = CLOCK_TOLERANCE;
+		// Do you want to automatically make somebody an admin? This line might help you do that...
+		// profile.isAdmin = data.isAdmin ? true : false;
 
-		const strategy = new Strategy({
-			client,
-			params: {
-				// In OpenID Connect,
-				// => issuer and subject in the 'openid' scope
-				// => email in the 'email' scope
-				// => username in the 'profile' scope ( as 'preferred_username' )
-				// scope: 'openid email profile'
+		// Delete or comment out the next TWO (2) lines when you are ready to proceed
+		process.stdout.write('===\nAt this point, you\'ll need to customise the above section to id, displayName, and emails into the "profile" object.\n===');
+		return callback(new Error('Congrats! So far so good -- please see server log for details'));
+
+		// eslint-disable-next-line
+		callback(null, profile);
+	};
+
+	OAuth.login = async (payload) => {
+		let uid = await OAuth.getUidByOAuthid(payload.oAuthid);
+		if (uid !== null) {
+			// Existing User
+			return ({
+				uid: uid,
+			});
+		}
+
+		// Check for user via email fallback
+		uid = await User.getUidByEmail(payload.email);
+		if (!uid) {
+			// New user
+			uid = await User.create({
+				username: payload.handle,
+				email: payload.email,
+			});
+		}
+
+		// Save provider-specific information to the user
+		await User.setUserField(uid, constants.name + 'Id', payload.oAuthid);
+		await db.setObjectField(constants.name + 'Id:uid', payload.oAuthid, uid);
+
+		if (payload.isAdmin) {
+			await Groups.join('administrators', uid);
+		}
+
+		return {
+			uid: uid,
+		};
+	};
+
+	OAuth.getUidByOAuthid = async (oAuthid) => db.getObjectField(constants.name + 'Id:uid', oAuthid);
+
+	OAuth.deleteUserData = function (data, callback) {
+		async.waterfall([
+			async.apply(User.getUserField, data.uid, constants.name + 'Id'),
+			function (oAuthIdToDelete, next) {
+				db.deleteObjectField(constants.name + 'Id:uid', oAuthIdToDelete, next);
 			},
-		}, verify);
+		], function (err) {
+			if (err) {
+				winston.error('[sso-oauth] Could not remove OAuthId data for uid ' + data.uid + '. Error: ' + err);
+				return callback(err);
+			}
 
-		passport.use(strategy.name, strategy);
-
-		strategies.push({
-			name: strategy.name,
-			url: AUTH_OIDC_LOGIN_PATH,
-			callbackURL: AUTH_OIDC_CALLBACK_PATH,
-			icon: 'fa-key',
-			scope: 'openid email profile',
+			callback(null, data);
 		});
+	};
 
-		winston.verbose('[oidc] Strategy initialized ...');
+	// If this filter is not there, the deleteUserData function will fail when getting the oauthId for deletion.
+	OAuth.whitelistFields = function (params, callback) {
+		params.whitelist.push(constants.name + 'Id');
+		callback(null, params);
+	};
 
-		callback(null, strategies);
-	} catch (err) {
-		winston.error(err);
-		callback(err);
-	}
-}
-
-module.exports = {
-	init,
-	addMenuItem,
-	getStrategy,
-};
+	module.exports = OAuth;
+}(module));
